913f0754551827f3f871e4084722c1d5fde19c40
devopmod <devopmod@gmail.com>
2025-08-13 11:15:24 +0200

    feat: embed font for pdf generation

--- src/utils/pdfGenerator.test.js
+++ src/utils/pdfGenerator.test.js
@@ -1,15 +1,35 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { PDFDocument, StandardFonts } from 'pdf-lib';
+import { PDFDocument } from 'pdf-lib';
+import fontkit from '@pdf-lib/fontkit';
 import { drawTicketPage } from './pdfGenerator.js';
 
 test('drawTicketPage adds a page to the PDF document', async () => {
   const pdfDoc = await PDFDocument.create();
-  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  pdfDoc.registerFontkit(fontkit);
+  const fontBytes = await fetch('https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf').then((res) =>
+    res.arrayBuffer()
+  );
+  const font = await pdfDoc.embedFont(fontBytes, { subset: true });
   const order = { event: {} };
   const settings = { design: { showQRCode: false } };
 
   await drawTicketPage(pdfDoc, order, null, settings, font);
 
   assert.equal(pdfDoc.getPageCount(), 1);
+});
+
+test('drawTicketPage supports Cyrillic characters', async () => {
+  const pdfDoc = await PDFDocument.create();
+  pdfDoc.registerFontkit(fontkit);
+  const fontBytes = await fetch('https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf').then((res) =>
+    res.arrayBuffer()
+  );
+  const font = await pdfDoc.embedFont(fontBytes, { subset: true });
+  const order = { event: { artist: 'Привет мир!' } };
+  const settings = { design: { showQRCode: false } };
+
+  await drawTicketPage(pdfDoc, order, null, settings, font);
+  const pdfBytes = await pdfDoc.save();
+  assert.ok(pdfBytes.byteLength > 0);
 });
